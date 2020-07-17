b1092a198f4d6af9c6fcfaf83e76cf8b0df6b6ad
davedjoh <ivandidjohgah@gmail.com>
2020-07-18 00:03:53 +0800

    make user dashboard view part 1

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -19,7 +19,7 @@ class Admin extends CI_Controller
         $this->load->view('layout/app/app_header', $data);
         $this->load->view('layout/app/app_sidebar', $data);
         $this->load->view('layout/app/app_topbar', $data);
-        $this->load->view('modul/user/index', $data);
+        $this->load->view('modul/admin/index', $data);
         $this->load->view('layout/app/app_footer');
     }
 }
--- application/views/modul/user/index.php
+++ application/views/modul/user/index.php
@@ -12,10 +12,14 @@
                 <div class="d-sm-flex align-items-center justify-content-between mb-4">
                     <h1 class="h3 mb-0 text-gray-800"><?= $title; ?></h1>
                 </div>
-
                 <!-- Content Row -->
             </div>
 
+            <div class="row">
+                <div class="col-md-12">
+                    <?= $profiles['name']; ?>
+                </div>
+            </div>
 
 
         </div>
