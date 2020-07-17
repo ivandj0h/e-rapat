16f772435d3bc72bcc6a4f40fa1d5dd6cde64b0c
ivandi1980 <ivandi.djoh@gmail.com>
2020-07-18 05:17:56 +0800

    make admin dashboard view part 3 - modified dashboar table & export sql

--- application/models/DashboardModel.php
+++ application/models/DashboardModel.php
@@ -9,11 +9,16 @@ class DashboardModel extends CI_Model
         parent::__construct();
     }
 
-    public function get_meeting_hari_ini()
+    public function get_todays_meeting()
     {
         $date = new DateTime("now");
         $curr_date = $date->format('Y-m-d ');
 
         return $this->db->get_where('view_meeting', ['DATE(date_issues)' => $curr_date])->result_array();
+    }
+
+    public function get_count_status()
+    {
+        return $this->db->get('view_total_request_status')->result_array();
     }
 }
