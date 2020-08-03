1d2d0587546ca252a3da9f1020a597feaf2adff3
davedjoh <ivandidjohgah@gmail.com>
2020-08-03 12:12:08 +0700

    db modified

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ date_default_timezone_set("Asia/Jakarta");
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/rapat/';
+$config['base_url'] = 'http://10.151.0.197/rapat/';
 
 /*
 |--------------------------------------------------------------------------
