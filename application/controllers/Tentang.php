5cb4fe04c575b212f595067df48b2c955321f118
davedjoh <ivandidjohgah@gmail.com>
2020-08-19 15:48:34 +0700

    Fullstack Part 4 - Calendar frontend modified

--- application/controllers/Tentang.php
+++ application/controllers/Tentang.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class About extends CI_Controller
+class Tentang extends CI_Controller
 {
 
 	public function __construct()
--- application/views/frontend/about.php
+++ application/views/frontend/about.php
@@ -1,6 +1,6 @@
-<div class="col-lg-10" style="float:none;margin:auto;overflow: auto;padding-top: 50px;">
+<div class="col-lg-10 mt-3" style="float:none;margin:auto;overflow: auto;padding-top: 50px;">
   <div class="jumbotron">
-    <h1 class="display-4">Hello, world!</h1>
+    <h1 class="display-4">Tentang!</h1>
     <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
     <hr class="my-4">
 
