4f3d387f68d4ed812ef44e86a9f661a03da8cdbb
ivandi1980 <email@address.com>
2020-09-18 18:47:42 +0700

    Legacy_Update - Create Online Docs

--- application/views/layout/front_topbar.php
+++ application/views/layout/front_topbar.php
@@ -3,7 +3,7 @@
         <h5 class="my-0 mr-md-auto font-weight-normal"><a href=""><i class="fas fa-calendar-alt"></i> <?= $title; ?></a></h5>
         <nav class="my-2 my-md-0 mr-md-3">
             <a class="p-2 text-dark text-decoration-none" href="<?= base_url('beranda') ?>">Beranda</a>
-            <!-- <a class="p-2 text-dark text-decoration-none" href="<?= base_url('berita') ?>">Berita</a> -->
+            <a class="p-2 text-dark text-decoration-none" href="<?= base_url('dokumentasi') ?>">Dokumentasi</a>
         </nav>
         <a class="btn btn-outline-success" href="<?= base_url('auth') ?>">
             <?php
