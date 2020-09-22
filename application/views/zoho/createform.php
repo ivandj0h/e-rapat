2fd90bb6328305b12cabe4b2a088204f86527d04
ivandi1980 <ivandi.djoh@gmail.com>
2020-09-22 16:39:11 +0700

    Legacy_Update - Modified customs modals

--- application/views/zoho/createform.php
+++ application/views/zoho/createform.php
@@ -11,9 +11,10 @@
         <!-- Start Content Table -->
         <div class="row">
             <div class="col-lg-12">
-                <iframe src="https://forms.zoho.com/projectlitbang/form/FormAbsensiRapat/builder" style="position: fixed; left:100px; bottom:0; right: 0; width: 102%; height: 97%; border: none; margin:0; overflow:hidden; z-index:1;">
+                <!-- <iframe frameborder="0" src="https://accounts.zoho.com/signin?servicename=ZohoForms&signupurl=https://www.zoho.com/forms/signup.html&serviceurl=https://forms.zoho.com" style="position: fixed; left:100px; bottom:0; right: 0; width: 102%; height: 97%; border: none; margin:0; overflow:hidden; z-index:1;">
                     Your browser doesn't support iframes
-                </iframe>
+                </iframe> -->
+                <iframe src="https://accounts.zoho.com/signin?servicename=ZohoForms&signupurl=https://www.zoho.com/forms/signup.html&serviceurl=https://forms.zoho.com" style="position: fixed; left:100px; bottom:0; right: 0; width: 102%; height: 97%; border: none; margin:0; overflow:hidden; z-index:1;"></iframe>
             </div>
         </div>
         <!-- End of Content Table -->
