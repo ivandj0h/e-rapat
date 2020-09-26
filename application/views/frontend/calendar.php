f56acd18868983158d530c9e5db56ce4131b985b
davedjoh <ivandidjohgah@gmail.com>
2020-09-26 18:02:25 +0700

    Legacy update - added xframeoptions'

--- application/views/frontend/calendar.php
+++ application/views/frontend/calendar.php
@@ -1,5 +1,5 @@
 <div class="col-lg-12" style="float:none;margin:auto;">
-  <iframe src="<?= base_url('calendar') ?>" style="position: fixed; padding-top:75px; left:0;bottom:0; right: 0; width: 100%; height: 100%; border: none; margin:0; overflow-x: hidden; z-index:1;">
+  <iframe src="<?= base_url('calendar') ?>" style="position: fixed; padding-top:75px; left:0;bottom:0; right: 0; width: 100%; height: 100%; border: none; margin:0; overflow-x: hidden; z-index:0;">
     Your browser doesn't support iframes
   </iframe>
 </div>
